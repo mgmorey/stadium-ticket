d690a4160151e4c6bd7257bd5cafe450da503e9c
mgmorey <mgmorey@gmail.com>
2018-11-18 12:27:31 -0800

    Combine import statements from package sqlalchemy.

--- tickets.py
+++ tickets.py
@@ -4,8 +4,7 @@ import datetime
 import json
 import os
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
