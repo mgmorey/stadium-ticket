21be206fa509a87ea10db380d717024653c109cd
mgmorey <mgmorey@gmail.com>
2018-11-18 13:58:37 -0800

    Rename install-prequisites.sh to install-prerequisites.sh.

--- install-prequisites.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-python3 -m pip install --user -r requirements.txt
---
+++ install-prerequisites.sh
@@ -0,0 +1,65 @@
+#!/bin/sh -eux
+
+# install-prereuisites: install prerequisites
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DEBIAN_PKGS="apache2-utils mariadb-server python3 python3-pip python3-flask \
+python3-pymysql python3-sqlalchemy"
+FEDORA_PKGS="apache2-utils mariadb python3 python3-flask python3-PyMySQL \
+python3-sqlalchemy"
+FREEBSD_PKGS="apache24 mysql56-server python3 py36-pip py36-Flask py36-pymysql \
+py36-sqlalchemy"
+ILLUMOS_PKGS="apache-24 mariadb-101 python-34 pip-34 sqlalchemy"
+SUSE_PKGS="apache2-utils mariadb python3 python3-Flask python3-PyMySQL\
+python3-sqlalchemy"
+
+abort() {
+    printf "$@" >&2
+    exit 1
+}
+
+distro_name=$(get-os-distro-name)
+kernel_name=$(get-os-kernel-name)
+
+case "$kernel_name" in
+    (Linux)
+	case "$distro_name" in
+	    (debian|ubuntu)
+		sudo apt-get install "$@" $DEBIAN_PKGS
+		;;
+	    (fedora)
+		sudo dnf install "$@" $DEBIAN_PKGS
+		;;
+	    (opensuse-*)
+		sudo zypper install "$@" $SUSE_PKGS
+		;;
+	    (*)
+		abort "%s: Distro not supported\n" "$distro_name"
+		;;
+	esac
+	;;
+    (FreeBSD)
+	sudo pkg install "$@" $FREEBSD_PKGS
+	;;
+    (SunOS)
+	sudo pkg install "$@" $ILLUMOS_PKGS
+	;;
+    (*)
+	abort "%s: Operating system not supported\n" "$kernel_name"
+	;;
+esac
+
+python3 -m pip install --user -r requirements.txt
