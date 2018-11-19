58c376ef0b37d78e0a0d3b950664b269b2b7d915
mgmorey <mgmorey@gmail.com>
2018-11-18 19:59:13 -0800

    Add new script file get-prerequisites.sh.

---
+++ get-prerequisites.sh
@@ -0,0 +1,68 @@
+#!/bin/sh -eu
+
+# install-prerequisites: install prerequisites
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DEBIAN_DBMS="mariadb-server python3-pymysql python3-sqlalchemy"
+DEBIAN_PKGS="apache2-utils python3 python3-pip $DEBIAN_DBMS python3-flask"
+
+FEDORA_DBMS="mariadb python3-PyMySQL python3-sqlalchemy"
+FEDORA_PKGS="httpd-tools python3 python3-pip $FEDORA_DBMS python3-flask"
+
+FREEBSD_DBMS="mysql56-server py36-pymysql py36-sqlalchemy12"
+FREEBSD_PKGS="apache24 python3 py36-pip $FREEBSD_DBMS py36-Flask"
+
+OPENSUSE_DBMS="mariadb python3-PyMySQL python3-SQLAlchemy"
+OPENSUSE_PKGS="apache2-utils python3 python3-pip $OPENSUSE_DBMS python3-Flask"
+
+SUNOS_DBMS="mariadb-101 sqlalchemy-34"
+SUNOS_PKGS="apache-24 python-34 pip-34 $SUNOS_DBMS"
+
+abort() {
+    printf "$@" >&2
+    exit 1
+}
+
+distro_name=$(get-os-distro-name)
+kernel_name=$(get-os-kernel-name)
+
+case "$kernel_name" in
+    (Linux)
+	case "$distro_name" in
+	    (debian|ubuntu)
+		printf "$DEBIAN_PKGS"
+		;;
+	    (fedora)
+		printf "$FEDORA_PKGS"
+		;;
+	    (opensuse-*)
+		printf "$OPENSUSE_PKGS"
+		;;
+	    (*)
+		abort "%s: Distro not supported\n" "$distro_name"
+		;;
+	esac
+	;;
+    (FreeBSD)
+	printf "$FREEBSD_PKGS"
+	;;
+    (SunOS)
+	printf "$SUNOS_PKGS"
+	;;
+    (*)
+	abort "%s: Operating system not supported\n" "$kernel_name"
+	;;
+esac
