0a85bd53cf40b3db55e6f19f4ebd6411b28afd65
mgmorey <mgmorey@gmail.com>
2018-11-18 19:23:20 -0800

    Correct typo in comments.

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -u
 
-# install-prereuisites: install prerequisites
+# install-prerequisites: install prerequisites
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
