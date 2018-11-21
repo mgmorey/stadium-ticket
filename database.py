c62eb31661e87fc1b395c32059f21943df710273
mgmorey <mgmorey@gmail.com>
2018-11-21 08:39:32 -0800

    Remove unused import.

--- database.py
+++ database.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
