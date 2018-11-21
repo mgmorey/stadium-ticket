b9a6f617ecb0061d56cf3aece06d28dba9033852
mgmorey <mgmorey@gmail.com>
2018-11-21 08:35:01 -0800

    Factor out computation of connection URL into module connection.py.

--- database.py
+++ database.py
@@ -6,20 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-CONNECTION = "{0}+{1}://{2}:{3}@{4}/{5}"
-DIALECT = 'mysql'
-DRIVER = 'pymysql'
-HOST = 'localhost'
-SCHEMA = 'stadium-tickets'
-
-
-def get_connection():
-    return CONNECTION.format(DIALECT, DRIVER,
-                             os.getenv('MYSQL_USER', os.getenv('USER')),
-                             os.getenv('MYSQL_PASSWORD'),
-                             os.getenv('MYSQL_HOST', HOST),
-                             SCHEMA)
-
+from connection import get_connection
 
 engine = create_engine(get_connection())
 session = scoped_session(sessionmaker(bind=engine))
