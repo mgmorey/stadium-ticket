01fa15bf3c43f365915294f7990577e197865853
mgmorey <mgmorey@gmail.com>
2018-11-26 13:21:34 -0800

    Rename script install-prerequisites.sh to install-dependencies.sh.

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -u
 
-# install-prerequisites: install prerequisite packages for building app
+# install-dependencies: install prerequisites for developing app
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
