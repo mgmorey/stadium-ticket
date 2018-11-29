e9545f304e82cd04cc90e4100473e247e5f5c5bd
mgmorey <mgmorey@gmail.com>
2018-11-28 21:56:10 -0800

    Add new script file get-dependencies.sh.

---
+++ scripts/get-dependencies.sh
@@ -0,0 +1,51 @@
+#!/bin/sh -eu
+
+# get-dependencies: get list of prerequisites for developing app
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+abort() {
+    printf "$@" >&2
+    exit 1
+}
+
+get_deps() {
+    for category in database-client http-client middleware; do
+	printf "%s\n" "$script_dir/get-$category-packages.sh"
+    done | sh
+}
+
+distro_name=$(get-os-distro-name)
+kernel_name=$(get-os-kernel-name)
+script_dir=$(dirname $0)
+
+case "$kernel_name" in
+    (Linux)
+	case "$distro_name" in
+	    (debian|ubuntu|centos|fedora|readhat|opensuse-*)
+		get_deps
+		;;
+	    (*)
+		abort "%s: Distro not supported\n" "$distro_name"
+		;;
+	esac
+	;;
+    (FreeBSD|SunOS)
+	get_deps
+	;;
+    (*)
+	abort "%s: Operating system not supported\n" "$kernel_name"
+	;;
+esac
