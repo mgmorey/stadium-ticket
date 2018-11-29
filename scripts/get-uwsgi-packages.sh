7c2f962c10f50d951d83239711185a71bed615ea
mgmorey <mgmorey@gmail.com>
2018-11-28 17:01:59 -0800

    Add new script file get-uwsgi-packages.sh.

---
+++ scripts/get-uwsgi-packages.sh
@@ -0,0 +1,65 @@
+#!/bin/sh -eu
+
+# get-uwsgi-packages: get uWSGI app server package names
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DEBIAN_PKGS="uwsgi uwsgi-plugin-python3"
+
+FEDORA_PKGS=""
+
+FREEBSD_PKGS=""
+
+OPENSUSE_PKGS=""
+
+REDHAT_PKGS=""
+
+SUNOS_PKGS=""
+
+UBUNTU_PKGS="uwsgi uwsgi-plugin-python3"
+
+distro_name=$(get-os-distro-name)
+kernel_name=$(get-os-kernel-name)
+
+case "$kernel_name" in
+    (Linux)
+	case "$distro_name" in
+	    (debian)
+		packages=$DEBIAN_PKGS
+		;;
+	    (fedora)
+		packages=$FEDORA_PKGS
+		;;
+	    (redhat|centos)
+		packages=$REDHAT_PKGS
+		;;
+	    (opensuse-*)
+		packages=$OPENSUSE_PKGS
+		;;
+	    (ubuntu)
+		packages=$UBUNTU_PKGS
+		;;
+	esac
+	;;
+    (FreeBSD)
+	packages=$FREEBSD_PKGS
+	;;
+    (SunOS)
+	packages=$SUNOS_PKGS
+	;;
+esac
+
+
+printf "%s\n" $packages
