1b69287ef4dea78c9b097ab66649cca3e72654b4
mgmorey <mgmorey@gmail.com>
2018-12-01 09:53:03 -0800

    Use format strings to construct dialect + driver.

--- app/database/uri.py
+++ app/database/uri.py
@@ -8,14 +8,20 @@ SCHEMA = 'stadium-tickets'
 
 
 def get_uri():
-    host = config('MYSQL_HOST')
+    dialect = DIALECT
+    driver = DRIVER
+
+    if driver:
+        dialect = f"{dialect}+{driver}"
+
     user = config('MYSQL_USER')
+    host = config('MYSQL_HOST')
     password = config('MYSQL_PASSWORD')
 
     if password:
-        user = f'{user}:{password}'
+        user = f"{user}:{password}"
 
     if host:
-        user = f'{user}@{host}'
+        user = f"{user}@{host}"
 
-    return f"{DIALECT}+{DRIVER}://{user}/{SCHEMA}"
+    return f"{dialect}://{user}/{SCHEMA}"
