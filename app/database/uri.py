d73fa125a239212b35517ce4d82f08aec6cb051e
mgmorey <mgmorey@gmail.com>
2018-12-02 13:34:08 -0800

    Refactor to use intelligent default for driver based on SQL dialect.

--- app/database/uri.py
+++ app/database/uri.py
@@ -5,21 +5,34 @@ import os
 from decouple import config
 
 DIALECT = 'mysql'
-DRIVER = 'pymysql'
-HOST = 'mysql'
-USER = os.getenv('USER')
+DRIVERS = {
+    'mysql': 'py{0}'
+}
+HOST = 'localhost'
+USER = 'root'
 PASSWORD = ''
 SCHEMA = 'stadium-tickets'
-URI_DEFAULT = "{0}://{1}/{2}"
 URIS = {
 }
 
+def _default_driver(dialect: str):
+    format = DRIVERS.get(dialect)
+    return format.format(dialect) if format else None
+
+def _default_format(dialect: str):
+    return URIS.get(dialect, "{0}://{1}/{2}")
+
+def _default_user():
+    return os.getenv('USER', 'root')
+
 def get_uri():
     dialect = config('DATABASE_DIALECT', default=DIALECT)
-    driver = config('DATABASE_DRIVER', default=DRIVER)
+    driver = config('DATABASE_DRIVER', default=_default_driver(dialect))
+    format = config('DATABASE_URI', default=_default_format(dialect))
     hostname = config('DATABASE_HOST', default=HOST)
-    username = config('DATABASE_USER', default=USER)
+    username = config('DATABASE_USER', default=_default_user())
     password = config('DATABASE_PASSWORD', default=PASSWORD)
+    schema = config('DATABASE_SCHEMA', default=SCHEMA)
 
     if driver:
         dialect = f"{dialect}+{driver}"
@@ -30,6 +43,4 @@ def get_uri():
     if hostname:
         username = f"{username}@{hostname}"
 
-    schema = config('DATABASE_SCHEMA', default=SCHEMA)
-    uri = config('DATABASE_URI', default=URIS.get(schema, URI_DEFAULT))
-    return uri.format(dialect, username, schema)
+    return format.format(dialect, username, schema)
