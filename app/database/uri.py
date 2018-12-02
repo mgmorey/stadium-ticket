73e56c844feab533c17d712bc57bed8d0c46558a
mgmorey <mgmorey@gmail.com>
2018-12-02 15:21:46 -0800

    Reduce the number of default parameters.

--- app/database/uri.py
+++ app/database/uri.py
@@ -4,15 +4,14 @@ import os
 
 from decouple import config
 
-DIALECT = 'mysql'
 DRIVER = {
     'mysql': 'py{0}'
 }
 HOST = 'localhost'
 USER = 'root'
-PASSWORD = None
 SCHEMA = 'stadium-tickets'
 URI = {
+    None: "{0}://{1}@{2}/{3}"
 }
 
 
@@ -21,9 +20,9 @@ def _default_driver(dialect: str):
     return driver.format(dialect) if driver else None
 
 
-def _get_credentials():
-    password = config('DATABASE_PASSWORD', default=PASSWORD)
-    user = config('DATABASE_USER', default=os.getenv('USER', 'root'))
+def _get_login():
+    password = config('DATABASE_PASSWORD')
+    user = config('DATABASE_USER', default=os.getenv('USER', USER))
     return f"{user}:{password}" if password else user
 
 
@@ -32,17 +31,17 @@ def _get_scheme(dialect: str):
     return f"{dialect}+{driver}" if driver else dialect
 
 
-def _get_uri(dialect: str, credentials: str, host: str, schema: str):
-    s = config('DATABASE_URI', default=URI.get(dialect, "{0}://{1}@{2}/{3}"))
-    return s.format(_get_scheme(dialect), credentials, host, schema)
+def _get_uri(dialect: str, login: str, host: str, schema: str):
+    s = config('DATABASE_URI', default=URI.get(dialect, URI[None]))
+    return s.format(_get_scheme(dialect), login, host, schema)
 
 
 def get_uri():
-    credentials = _get_credentials()
-    dialect = config('DATABASE_DIALECT', default=DIALECT)
+    login = _get_login()
+    dialect = config('DATABASE_DIALECT')
     host = config('DATABASE_HOST', default=HOST)
     schema = config('DATABASE_SCHEMA', default=SCHEMA)
-    return _get_uri(dialect, credentials, host, schema)
+    return _get_uri(dialect, login, host, schema)
 
 
 if __name__ == '__main__':
