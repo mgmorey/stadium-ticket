d468e56fc5b87382d3ece58b9323444cfcd6f7d5
mgmorey <mgmorey@gmail.com>
2018-12-02 14:43:02 -0800

    Refactor by dividing into smaller methods.

--- app/database/uri.py
+++ app/database/uri.py
@@ -10,7 +10,7 @@ DRIVERS = {
 }
 HOST = 'localhost'
 USER = 'root'
-PASSWORD = ''
+PASSWORD = None
 SCHEMA = 'stadium-tickets'
 URIS = {
 }
@@ -19,28 +19,25 @@ def _default_driver(dialect: str):
     driver = DRIVERS.get(dialect)
     return driver.format(dialect) if driver else None
 
-def _default_format(dialect: str):
-    return URIS.get(dialect, "{0}://{1}/{2}")
+def _default_uri(dialect: str):
+    return URIS.get(dialect, "{0}://{1}@{2}/{3}")
 
-def _default_user():
-    return os.getenv('USER', 'root')
+def _get_credentials():
+    password = config('DATABASE_PASSWORD', default=PASSWORD)
+    username = config('DATABASE_USER', default=os.getenv('USER', 'root'))
+    return f"{username}:{password}" if password else username
+
+def _get_scheme(dialect: str):
+    driver = config('DATABASE_DRIVER', default=_default_driver(dialect))
+    return f"{dialect}+{driver}" if driver else dialect
+
+def _get_uri(dialect: str, credentials: str, host: str, schema: str):
+    uri = config('DATABASE_URI', default=_default_uri(dialect))
+    return uri.format(_get_scheme(dialect), credentials, host, schema)
 
 def get_uri():
+    credentials = _get_credentials()
     dialect = config('DATABASE_DIALECT', default=DIALECT)
-    driver = config('DATABASE_DRIVER', default=_default_driver(dialect))
     hostname = config('DATABASE_HOST', default=HOST)
-    username = config('DATABASE_USER', default=_default_user())
-    password = config('DATABASE_PASSWORD', default=PASSWORD)
     schema = config('DATABASE_SCHEMA', default=SCHEMA)
-    uri = config('DATABASE_URI', default=_default_format(dialect))
-
-    if driver:
-        dialect = f"{dialect}+{driver}"
-
-    if password:
-        username = f"{username}:{password}"
-
-    if hostname:
-        username = f"{username}@{hostname}"
-
-    return uri.format(dialect, username, schema)
+    return _get_uri(dialect, credentials, hostname, schema)
