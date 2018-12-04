292991da6c96761332c4ffdb37e18eab61470685
mgmorey <mgmorey@gmail.com>
2018-12-04 12:08:59 -0800

    Do not retrieve host and login information for dialect sqlite.

--- app/database/uri.py
+++ app/database/uri.py
@@ -21,7 +21,17 @@ def _get_driver(dialect: str):
     return driver.format(dialect) if driver else None
 
 
-def _get_login():
+def _get_host(dialect: str):
+    if dialect == 'sqlite':
+        return None
+
+    return config('DATABASE_HOST', default=HOST)
+
+
+def _get_login(dialect: str):
+    if dialect == 'sqlite':
+        return None
+
     password = config('DATABASE_PASSWORD')
     user = config('DATABASE_USER', default=os.getenv('USER', USER))
     return f"{user}:{password}" if password else user
@@ -32,12 +42,18 @@ def _get_scheme(dialect: str):
     return f"{dialect}+{driver}" if driver else dialect
 
 
+def _get_uri(dialect: str):
+    return URI.get(dialect, URI[None])
+
+
 def get_uri():
     dialect = config('DATABASE_DIALECT')
-    host = config('DATABASE_HOST', default=HOST)
     schema = config('DATABASE_SCHEMA', default=SCHEMA)
-    s = config('DATABASE_URI', default=URI.get(dialect, URI[None]))
-    return s.format(_get_scheme(dialect), _get_login(), host, schema)
+    s = config('DATABASE_URI', default=_get_uri(dialect))
+    return s.format(_get_scheme(dialect),
+                    _get_login(dialect),
+                    _get_host(dialect),
+                    schema)
 
 
 if __name__ == '__main__':
