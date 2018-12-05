a08334b5c58d26eac07f99c07da2a5543c675e95
mgmorey <mgmorey@gmail.com>
2018-12-04 17:10:27 -0800

    Define function _get_config() to retrieve configuration parameters.

--- app/database/uri.py
+++ app/database/uri.py
@@ -16,6 +16,11 @@ URI = {
 }
 
 
+def _get_config(parameter: str, default: str = None):
+    value = config(parameter, default=default)
+    return value
+
+
 def _get_driver(dialect: str):
     driver = DRIVER.get(dialect)
     return driver.format(dialect) if driver else None
@@ -32,13 +37,13 @@ def _get_login(dialect: str):
     if '{3}' not in _get_uri(dialect):
         return None
 
-    password = config('DATABASE_PASSWORD')
-    user = config('DATABASE_USER', default=os.getenv('USER', USER))
+    password = _get_config('DATABASE_PASSWORD')
+    user = _get_config('DATABASE_USER', default=os.getenv('USER', USER))
     return f"{user}:{password}" if password else user
 
 
 def _get_scheme(dialect: str):
-    driver = config('DATABASE_DRIVER', default=_get_driver(dialect))
+    driver = _get_config('DATABASE_DRIVER', default=_get_driver(dialect))
     return f"{dialect}+{driver}" if driver else dialect
 
 
@@ -47,9 +52,9 @@ def _get_uri(dialect: str):
 
 
 def get_uri():
-    dialect = config('DATABASE_DIALECT')
-    schema = config('DATABASE_SCHEMA', default=SCHEMA)
-    uri = config('DATABASE_URI', default=_get_uri(dialect))
+    dialect = _get_config('DATABASE_DIALECT')
+    schema = _get_config('DATABASE_SCHEMA', default=SCHEMA)
+    uri = _get_config('DATABASE_URI', default=_get_uri(dialect))
     return uri.format(_get_scheme(dialect), schema,
                       _get_hostname(dialect),
                       _get_login(dialect))
