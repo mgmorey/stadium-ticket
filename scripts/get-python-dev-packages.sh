3ddb24bb2ef20c50adb82196bdcc5e4d489225f1
mgmorey <mgmorey@gmail.com>
2018-12-04 21:58:51 -0800

    Add new script file scripts/get-python-dev-packages.sh.

---
+++ scripts/get-python-dev-packages.sh
@@ -0,0 +1,80 @@
+#!/bin/sh -eu
+
+# get-python-dev-packages: get Python development package names
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+DEBIAN_PKGS="%s-dev %s-pip"
+
+FEDORA_PKGS="gcc libffi-devel openssl-devel %s-devel %s-pip"
+
+FREEBSD_PKGS="openssl-devel %s-pip"
+
+OPENSUSE_PKGS="gcc libffi-devel libopenssl-devel %s-devel %s-pip"
+
+REDHAT_PKGS="gcc libffi-devel openssl-devel %s-devel %s-pip"
+
+SUNOS_PKGS="build-essential pip-%s"
+
+UBUNTU_PKGS="build-essential libffi-dev libssl-dev %s-dev %s-pip"
+
+distro_name=$(get-os-distro-name)
+kernel_name=$(get-os-kernel-name)
+script_dir=$(dirname $0)
+
+case "$kernel_name" in
+    (Linux)
+	case "$distro_name" in
+	    (debian)
+		packages=$DEBIAN_PKGS
+		;;
+	    (fedora)
+		packages=$FEDORA_PKGS
+		;;
+	    (redhat|centos)
+		packages=$REDHAT_PKGS
+		;;
+	    (opensuse-*)
+		packages=$OPENSUSE_PKGS
+		;;
+	    (ubuntu)
+		packages=$UBUNTU_PKGS
+		;;
+	esac
+	;;
+    (FreeBSD)
+	packages=$FREEBSD_PKGS
+	;;
+    (SunOS)
+	packages=$SUNOS_PKGS
+	;;
+esac
+
+data=$($script_dir/get-python-package.sh)
+package_name=$(printf "%s" "$data" | awk '{print $1}')
+package_modifier=$(printf "%s" "$data" | awk '{print $2}')
+
+printf "%s\n" $package_name
+
+for package in $packages; do
+    case $package in
+	(*%s*)
+	    printf "$package\n" $package_modifier
+	    ;;
+	(*)
+	    printf "%s\n" $package
+	    ;;
+    esac
+done
