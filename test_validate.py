c75b8c72244d9eefdbf2f7c06de6630b9d118775
mgmorey <mgmorey@gmail.com>
2018-12-04 21:49:05 -0800

    Add test cases to unit test suite.

--- test_validate.py
+++ test_validate.py
@@ -8,17 +8,55 @@ from app.database.validate import validate_string
 
 class TestValidateFunctions(unittest.TestCase):
 
-    def test_validate_dialect(self):
-        validate_string('DATABASE_DIALECT', 'mysql')
+    def test_validate_dialect_fail(self):
+        self.assertRaises(ValueError,
+                          validate_string,
+                          'DATABASE_DIALECT',
+                          'mysql!')
 
-    def test_validate_host(self):
-        validate_string('DATABASE_HOST', 'localhost')
+    def test_validate_dialect_pass(self):
+        value = validate_string('DATABASE_DIALECT', 'mysql')
+        self.assertEqual(value, 'mysql')
 
-    def test_validate_password(self):
-        validate_string('DATABASE_PASSWORD', 'Hello!')
+    def test_validate_host_fail(self):
+        self.assertRaises(ValueError,
+                          validate_string,
+                          'DATABASE_HOST',
+                          'localhost/')
 
-    def test_validate_user(self):
-        validate_string('DATABASE_DIALECT', 'root')
+    def test_validate_host_pass(self):
+        value = validate_string('DATABASE_HOST', 'localhost')
+        self.assertEqual(value, 'localhost')
+
+    def test_validate_password_fail(self):
+        self.assertRaises(ValueError,
+                          validate_string,
+                          'DATABASE_PASSWORD',
+                          'Hello!@')
+
+    def test_validate_password_pass(self):
+        value = validate_string('DATABASE_PASSWORD', 'Hello!')
+        self.assertEqual(value, 'Hello!')
+
+    def test_validate_schema_fail(self):
+        self.assertRaises(ValueError,
+                          validate_string,
+                          'DATABASE_SCHEMA',
+                          'Hello!@')
+
+    def test_validate_schema_pass(self):
+        value = validate_string('DATABASE_SCHEMA', 'my-database-schema')
+        self.assertEqual(value, 'my-database-schema')
+
+    def test_validate_user_fail(self):
+        self.assertRaises(ValueError,
+                          validate_string,
+                          'DATABASE_USER',
+                          'root:')
+
+    def test_validate_user_pass(self):
+        value = validate_string('DATABASE_USER', 'root')
+        self.assertEqual(value, 'root')
 
 
 if __name__ == '__main__':
