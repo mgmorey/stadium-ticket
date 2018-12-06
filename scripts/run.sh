38031a0db7f0407dde5aff932f6be67e48722a38
mgmorey <mgmorey@gmail.com>
2018-12-06 12:12:04 -0800

    Refactor script run.sh to create missing virtual environment.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -eu
+#!/bin/sh -eux
 
 # run: wrapper for invoking "pipenv run" if pipenv present
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
@@ -16,14 +16,46 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-script_dir=$(dirname $0)
-source_dir=$script_dir/..
+abort() {
+    printf "$@" >&2
+    exit 1
+}
 
-if which pipenv >/dev/null 2>&1; then
-    pipenv run "$@"
-elif . $source_dir/.env; then
-    export DATABASE_DIALECT DATABASE_HOST DATABASE_PASSWORD DATABASE_USER
-    export FLASK_APP FLASK_ENV
-    (. $source_dir/.venv/bin/activate
-     "$@")
+create_venv() {
+    (cd $source_dir
+
+     if [ ! -d .venv ]; then
+	 python3 -m venv .venv
+     fi
+
+     if [ -d .venv ]; then
+	 . .venv/bin/activate
+	 pip3 install --upgrade pip
+	 pip3 install -r requirements.txt
+    fi)
+}
+
+run_venv() {
+    if [ -d $source_dir/.venv ]; then
+	. $source_dir/.venv/bin/activate
+	. $source_dir/.env
+	export DATABASE_DIALECT DATABASE_HOST DATABASE_PASSWORD DATABASE_USER
+	export FLASK_APP FLASK_ENV
+	"$@"
+    else
+	abort "%s\n" "No available virtualenv"
+    fi
+}
+
+pipenv=$(which pipenv 2>/dev/null || true)
+source_dir=$(dirname $0)/..
+
+if [ -z "$pipenv" ]; then
+    create_venv
+fi
+
+if [ -n "$pipenv" ]; then
+    $pipenv run "$@"
+else
+    run_venv "$@"
 fi
