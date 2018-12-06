ede18ee881aa4c8f6765cf36250a658b14d7fc7a
mgmorey <mgmorey@gmail.com>
2018-12-05 18:28:13 -0800

    Modify first line of comments at the top of the file.

--- scripts/configure-app.sh
+++ scripts/configure-app.sh
@@ -1,6 +1,6 @@
 # -*- Mode: Shell-script -*-
 
-# configure-app.sh: configure uWSGI application
+# configure-app.sh: configure uWSGI application parameters
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
