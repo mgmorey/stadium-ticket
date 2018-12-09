4f1694c43e07cfd9a73751b3d1125ec0f9cf382b
mgmorey <mgmorey@gmail.com>
2018-12-09 23:42:27 +0000

    Fix typo in command line options to "pip3 install".

--- scripts/update-requirements.sh
+++ scripts/update-requirements.sh
@@ -31,5 +31,5 @@ else
     . .venv/bin/activate
     pip3 install --upgrade pip
     pip3 install -r requirements.txt
-    pip3 install -dr requirements-dev.txt
+    pip3 install -r requirements-dev.txt
 fi
