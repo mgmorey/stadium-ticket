dbf91df0633d99727b516797c58bae8588409c3d
mgmorey <mgmorey@gmail.com>
2018-12-09 19:24:46 -0800

    Run pip3 install with option -r for each of requirements*.txt.

--- scripts/run.sh
+++ scripts/run.sh
@@ -31,7 +31,7 @@ create_venv() {
      if [ -d .venv ]; then
 	 . .venv/bin/activate
 	 pip3 install --upgrade pip
-	 pip3 install -r requirements.txt
+	 pip3 install -r requirements.txt -r requirements-dev.txt
      fi)
 }
 
