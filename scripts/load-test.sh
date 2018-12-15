cbba1b73302706304b99e8ba8da745c36618a6e6
mgmorey <mgmorey@gmail.com>
2018-12-15 14:54:58 -0800

    Use localhost as default value for parameter DOCKER_HOST if empty.

--- scripts/load-test.sh
+++ scripts/load-test.sh
@@ -36,6 +36,7 @@ source_dir=$script_dir/..
 printf "%s\n" "Loading .env environment variables"
 . $source_dir/.env
 
+: ${DOCKER_HOST:=localhost}
 host=${FLASK_HOST-${DOCKER_HOST%:*}}
 port=${FLASK_PORT-5000}
 
