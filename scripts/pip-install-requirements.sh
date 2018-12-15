ef49ae43b6d408bf5c4fbe83446498e0efa2c50a
mgmorey <mgmorey@gmail.com>
2018-12-14 16:18:18 -0800

    Add new script file pip-install-requirements.sh.

---
+++ scripts/pip-install-requirements.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -eu
+
+# pip-install-requirements: update PIP and install requirements
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+PIP=pip3
+PYTHON=python3
+
+printf "%s\n" "Upgrading pip"
+pip="$(which $PYTHON) -m pip"
+$pip install --upgrade pip
+pip="$(which $PIP)"
+printf "%s\n" "Installing required packages"
+$pip install -r requirements.txt -r requirements-dev.txt
