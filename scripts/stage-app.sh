ee320ee4e30ef0b59de00a8fd74e4b0885c9cb31
mgmorey <mgmorey@gmail.com>
2018-12-16 08:32:27 -0800

    Add new script file stage-app.sh to stage uWSGI application.

---
+++ scripts/stage-app.sh
@@ -0,0 +1,61 @@
+#!/bin/sh -eu
+
+# stage-app.sh: stage uWSGI application
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+PYTHON=python3
+
+create_venv() {
+    if [ ! -d $virtualenv ]; then
+	printf "%s\n" "Creating virtual environment"
+	python3 -m venv $virtualenv
+    fi
+
+    if [ -d $virtualenv ]; then
+	. $script_dir/initialize-virtualenv.sh 
+    else
+	abort "%s\n" "No virtual environment"
+    fi
+}
+
+realpath() {
+    if [ -x /usr/bin/realpath ]; then
+	/usr/bin/realpath "$@"
+    else
+	if expr "$1" : '/.*' >/dev/null; then
+	    printf "%s\n" "$1"
+	else
+	    printf "%s\n" "$PWD/${1#./}"
+	fi
+    fi
+}
+
+# set default locales
+export LANG=${LANG:-en_US.UTF-8}
+export LC_ALL=${LC_ALL:-en_US.UTF-8}
+
+python=$(which $PYTHON)
+script_dir=$(realpath $(dirname $0))
+source_dir=$script_dir/..
+
+. "$script_dir/configure-app.sh"
+
+virtualenv=.venv-$APP_NAME
+cd "$source_dir"
+
+if [ $(id -u) -gt 0 ]; then
+    create_venv
+fi
