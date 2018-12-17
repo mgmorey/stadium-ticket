cb2ffbb5e8db87ab7ea16e27ee67877e844b2165
mgmorey <mgmorey@gmail.com>
2018-12-17 04:22:27 -0800

    Add new script file get-package-manager.sh.

---
+++ scripts/get-package-manager.sh
@@ -0,0 +1,69 @@
+#!/bin/sh -eu
+
+# get-package-manager: get name of package manager utility
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+abort() {
+    printf "$@" >&2
+    exit 1
+}
+
+realpath() {
+    if [ -x /usr/bin/realpath ]; then
+	/usr/bin/realpath "$@"
+    else
+	if expr "$1" : '/.*' >/dev/null; then
+	    printf "%s\n" "$1"
+	else
+	    printf "%s\n" "$PWD/${1#./}"
+	fi
+    fi
+}
+
+script_dir=$(realpath $(dirname $0))
+distro_name=$($script_dir/get-os-distro-name.sh)
+kernel_name=$($script_dir/get-os-kernel-name.sh)
+
+case "$kernel_name" in
+    (Linux)
+	case "$distro_name" in
+	    (debian|ubuntu)
+		printf apt-get
+		;;
+	    (fedora)
+		printf dnf
+		;;
+	    (redhat|centos)
+		printf yum
+		;;
+	    (opensuse-*)
+		printf zypper
+		;;
+	    (*)
+		abort "%s: Distro not supported\n" "$distro_name"
+		;;
+	esac
+	;;
+    (FreeBSD|SunOS)
+	printf pkg
+	;;
+    (Darwin)
+	printf brew
+	;;
+    (*)
+	abort "%s: Operating system not supported\n" "$kernel_name"
+	;;
+esac
