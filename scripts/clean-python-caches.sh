212e14a552c36a50d528c1869bd36df83c4ab2de
mgmorey <mgmorey@gmail.com>
2018-12-16 16:00:22 -0800

    Simplify regular expression for caches in clean-python-caches.sh.

--- scripts/clean-python-caches.sh
+++ scripts/clean-python-caches.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dirs=$(find . -type d \( -name '.venv*' -prune -o -name '*py*cache*' -print \))
+dirs=$(find . -type d \( -name '.venv*' -prune -o -name '__py*' -print \))
 
 if [ -n "$dirs" ]; then
     /bin/rm -rf $dirs
