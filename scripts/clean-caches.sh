bf5376a62b0a2a88624e7d3b9ab779f5e5586444
mgmorey <mgmorey@gmail.com>
2018-12-16 16:04:57 -0800

    Rename script clean-python-caches.sh and modify regular expression.

---
+++ scripts/clean-caches.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+dirs=$(find . -type d \( -name '.venv*' -prune -o -name __pycache__ -print \))
+
+if [ -n "$dirs" ]; then
+    /bin/rm -rf $dirs
+fi
--- scripts/clean-python-caches.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-dirs=$(find . -type d \( -name '.venv*' -prune -o -name '__py*' -print \))
-
-if [ -n "$dirs" ]; then
-    /bin/rm -rf $dirs
-fi
