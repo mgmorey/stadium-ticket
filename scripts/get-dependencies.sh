f9eed2181900ccbf324342d54eec15936c557fa9
mgmorey <mgmorey@gmail.com>
2018-12-25 15:09:18 -0800

    Remove dbms-client from CATEGORIES in script get-dependencies.sh.

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -16,8 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-CATEGORIES="dbms-client docker-client http-client \
-python-dev python-mw sqlite"
+CATEGORIES="docker-client http-client python-dev python-mw sqlite"
 
 abort() {
     printf "$@" >&2
