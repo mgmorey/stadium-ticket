633ba1aa0d96e06065214ef8e1f041ec0c6d8e47
mgmorey <mgmorey@gmail.com>
2018-12-27 13:29:33 -0800

    Add pattern .pytest_cache to find command line in clean-caches.sh.

--- scripts/clean-caches.sh
+++ scripts/clean-caches.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dirs=$(find . -type d \( -name '.venv*' -prune -o -name __pycache__ -print \))
+dirs=$(find . -type d \( -name '.venv*' -prune -o -name .pytest_cache -print -o -name __pycache__ -print \))
 
 if [ -n "$dirs" ]; then
     /bin/rm -rf $dirs
