0aea4e513e14bc901868dcb5d337fd1f218067f5
mgmorey <mgmorey@gmail.com>
2018-12-29 11:27:23 -0800

    Rename script lock-requirements.sh to pipenv-lock.sh.

--- scripts/pipenv-lock.sh
+++ scripts/pipenv-lock.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-# lock-requirements: generate requirements.txt using pipenv
+# pipenv-lock: generate package requirement lists using pipenv
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
