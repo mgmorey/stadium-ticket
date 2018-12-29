6dc4af9b2b02b773f3ead8d346f00f6fa40f1c0b
mgmorey <mgmorey@gmail.com>
2018-12-28 19:59:53 -0800

    Update comments in script lock-requirements.sh.

--- scripts/lock-requirements.sh
+++ scripts/lock-requirements.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-# lock-requirements: update requirements.txt using PIP
+# lock-requirements: generate requirements.txt using pipenv
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
