d2cf915a6cf5a5b3bcb1fa4a879919f89ddaf613
mgmorey <mgmorey@gmail.com>
2018-12-30 17:46:47 -0800

    Migrate from Flask-Script to Click command line interface for Flask.

--- Dockerfile
+++ Dockerfile
@@ -44,7 +44,7 @@ USER $APP_UID
 ENV LANG=${LANG:-C.UTF-8}
 ENV LC_ALL=${LC_ALL:-C.UTF-8}
 ENV PIPENV_VENV_IN_PROJECT=true
-RUN pipenv sync && pipenv run python3 -m app init_db
+RUN pipenv sync && pipenv run python3 -m app init-db
 
 # Change to data directory, expose port and start app
 WORKDIR $APP_VARDIR
--- app/__main__.py
+++ app/__main__.py
@@ -3,19 +3,34 @@
 
 import logging
 
-from flask_script import Manager
+import click
 
 from .flask_app import LOGGING_FORMAT, app, db
 
-manager = Manager(app)  # pylint: disable=invalid-name
+
+@click.group()
+def cli():
+    """Create Click group named cli."""
 
 
-@manager.command
+@cli.command()
+def drop_db():
+    """Drop database schema and tables."""
+    click.echo('Dropping the database')
+    with app.app_context():
+        db.drop_all()
+    click.echo('Dropped the database')
+
+
+@cli.command()
 def init_db():
     """Create database schema and tables."""
+    click.echo('Initializing the database')
     with app.app_context():
         db.create_all()
+    click.echo('Initialized the database')
 
 
-logging.basicConfig(format=LOGGING_FORMAT, level=logging.DEBUG)
-manager.run()
+if __name__ == '__main__':
+    logging.basicConfig(format=LOGGING_FORMAT, level=logging.DEBUG)
+    cli()
