5039b54306f41ef6673c770d86454bc3a1de1e6a
Michael G. Morey <mgmorey@gmail.com>
2019-04-03 12:08:31 -0700

    Sort variable definitions near top of file.

--- scripts/update-virtualenv.sh
+++ scripts/update-virtualenv.sh
@@ -19,8 +19,8 @@
 export LANG=${LANG:-en_US.UTF-8}
 export LC_ALL=${LC_ALL:-en_US.UTF-8}
 
-PYTHON=python3
 PIP_VENV=.venv
+PYTHON=python3
 REQUIREMENTS="requirements-dev.txt requirements.txt"
 
 abort() {
