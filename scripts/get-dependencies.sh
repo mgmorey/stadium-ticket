29ee78dfc1ae7f2af312323c0c0852d7ed27f198
Michael G. Morey <mgmorey@gmail.com>
2019-04-05 18:41:42 -0700

    Remove category python-mw from list variable CATEGORIES.

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-CATEGORIES="http-client python-dev python-mw sqlite"
+CATEGORIES="http-client python-dev sqlite"
 
 abort() {
     printf "$@" >&2
