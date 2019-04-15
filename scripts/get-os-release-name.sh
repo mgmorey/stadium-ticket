b1e6e353be7eb33f100912127bed7a943595570f
Michael G. Morey <mgmorey@gmail.com>
2019-04-15 11:55:21 -0700

    Refactor scripts to use new script get-os-release-filenames.ch.

--- scripts/get-os-release-name.sh
+++ scripts/get-os-release-name.sh
@@ -16,39 +16,48 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-KEYS="os centos fedora redhat system"
+abort() {
+    printf "$@" >&2
+    exit 1
+}
 
+assert() {
+    "$@" || abort "%s: Assertion failed: %s\n" "$0" "$*"
+}
+
+realpath() {
+    assert [ -d "$1" ]
+
+    if [ -x /usr/bin/realpath ]; then
+	/usr/bin/realpath "$1"
+    else
+	if expr "$1" : '/.*' >/dev/null; then
+	    printf "%s\n" "$1"
+	else
+	    printf "%s\n" "$PWD/${1#./}"
+	fi
+    fi
+}
+
+script_dir=$(realpath "$(dirname "$0")")
 system_name="$(uname -s)"
 
 case "$system_name" in
     (Linux)
-	for key in $KEYS; do
-	    file=/etc/$key-release
+	files=$(sh -eu $script_dir/get-os-release-filenames.sh)
+	eval set -- $files
+	file=$1
 
-	    if [ -r $file ]; then
-		case ${file#/etc/} in
-		    (os-release)
-			if . $file && [ -n "${VERSION_ID:-}" ]; then
-			    printf "%s\n" "$VERSION_ID"
-			    exit 0
-			elif [ -x /usr/bin/lsb_release ]; then
-			    /usr/bin/lsb_release -rs
-			    exit 0
-			else
-			    exit 1
-			fi
-			;;
-		    (*-release)
-			awk '{print $4}' $file
-			exit 0
-			;;
-		esac
-	    fi
-	done
-
-	if [ -x /usr/bin/lsb_release ]; then
-	    /usr/bin/lsb_release -rs
-	fi
+	case ${file#/etc/} in
+	    (os-release)
+		if . $file && [ -n "${VERSION_ID:-}" ]; then
+		    printf "%s\n" "$VERSION_ID"
+		fi
+		;;
+	    (*)
+		abort "%s: Unknown release file\n" "$file"
+		;;
+	esac
 	;;
     (Darwin|FreeBSD|GNU|Minix)
 	uname -r
@@ -57,6 +66,7 @@ case "$system_name" in
 	awk 'NR == 1 {print $3}' /etc/release
 	;;
     (CYGWIN_NT-*)
-	uname -r
+	release_name=${system_name#*-}
+	printf "%s\n" "$release_name"
 	;;
 esac
