46633761cb285376424bf2f3706535c15e5c53db
Michael G. Morey <mgmorey@gmail.com>
2019-04-20 17:06:09 -0700

    Add new script file get-installed-dbms-package.sh.

---
+++ scripts/get-installed-dbms-package.sh
@@ -0,0 +1,58 @@
+#!/bin/sh -eu
+
+# get-installed-dbms-package: get installed database package name
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+abort() {
+    printf "$@" >&2
+    exit 1
+}
+
+assert() {
+    "$@" || abort "%s: Assertion failed: %s\n" "$0" "$*"
+}
+
+realpath() {
+    assert [ -d "$1" ]
+
+    if [ -x /usr/bin/realpath ]; then
+	/usr/bin/realpath "$1"
+    else
+	if expr "$1" : '/.*' >/dev/null; then
+	    printf "%s\n" "$1"
+	else
+	    printf "%s\n" "$PWD/${1#./}"
+	fi
+    fi
+}
+
+if [ $# -ne 1 ]; then
+    abort "%s: Invalid number of arguments\n" "$0"
+elif [ "$1" != client -a "$1" != server ]; then
+    abort "%s: Invalid argument -- %s\n" "$0" "$1"
+fi
+
+mode=$1
+script_dir=$(realpath "$(dirname "$0")")
+
+tmpfile=$(mktemp)
+trap "/bin/rm -f $tmpfile" EXIT INT QUIT TERM
+
+sh -eu $script_dir/get-installed-packages.sh >$tmpfile
+sh -eu $script_dir/grep-dbms-package.sh $mode-core <$tmpfile || \
+    sh -eu $script_dir/grep-dbms-package.sh $mode <$tmpfile || \
+    sh -eu $script_dir/grep-dbms-package.sh <$tmpfile || \
+    true
