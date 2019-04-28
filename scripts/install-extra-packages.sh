d32511fb4cde9a36bea09857c6aeb32c1686a609
Michael G. Morey <mgmorey@gmail.com>
2019-04-28 09:46:27 -0700

    Correct comment.

--- scripts/install-extra-packages.sh
+++ scripts/install-extra-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-# install-dependencies: install extra packages
+# install-extra-packages: install extra packages
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
