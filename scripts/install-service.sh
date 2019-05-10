4a3cd36d159c06fcd2aa3f472f98cdbda67ec383
Michael G. Morey <mgmorey@gmail.com>
2019-05-10 13:29:27 -0700

    Refactor shell functions and enclose script paths in double quotes.

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -51,28 +51,12 @@ create_dirs() {
 create_symlinks() (
     assert [ $# -ge 1 ]
     assert [ -n "$1" ]
-    source=$1
+    file=$1
     shift
 
-    for target_dir in "$@"; do
-	create_symlink $source $UWSGI_ETCDIR/$target_dir/$APP_NAME.ini
+    for dir in "$@"; do
+	create_symlink $file $UWSGI_ETCDIR/$dir/$APP_NAME.ini
     done
-)
-
-create_uwsgi_ini() (
-    assert [ $# -ge 3 ]
-    assert [ -n "$1" -a -n "$2" -a -r "$2" ]
-    target=$1
-    source=$2
-    shift 2
-    check_permissions $target
-
-    if [ $dryrun = false ]; then
-	printf "Generating configuration file %s\n" "$target"
-	mkdir -p "$(dirname $target)"
-	generate_sed_program "$@" >$tmpfile
-	sed -f $tmpfile $source >$target
-    fi
 )
 
 generate_sed_program() (
@@ -86,6 +70,19 @@ generate_sed_program() (
 	printf 's|^%s$|%s|g\n' "$pattern" "$replace"
     done
 )
+
+generate_service_ini() {
+    assert [ $# -eq 3 ]
+    assert [ -n "$1" -a -n "$2" -a -r "$2" -a "$3" ]
+    check_permissions $1
+
+    if [ $dryrun = false ]; then
+	printf "Generating configuration file %s\n" "$1"
+	mkdir -p "$(dirname $1)"
+	generate_sed_program $3 >$tmpfile
+	sed -f $tmpfile $2 >$1
+    fi
+}
 
 get_path() {
     assert [ -d "$1" ]
@@ -103,12 +100,9 @@ get_path() {
 install_flask_app() (
     assert [ $# -eq 3 ]
     assert [ -n "$1" -a -n "$2" -a -r "$2" -a -n "$3" ]
-    mode=$1
-    source_dir=$2
-    target_dir=$3
-    check_permissions $target_dir
+    check_permissions $3
 
-    for source in $(find $source_dir -type f -name '*.py' -print | sort); do
+    for source in $(find $2 -type f -name '*.py' -print | sort); do
 	case $source in
 	    (*/tests/*)
 		: # Omit tests folder
@@ -117,7 +111,7 @@ install_flask_app() (
 		: # Omit test modules
 		;;
 	    (*)
-		install_file $mode $source $target_dir/$source
+		install_file $1 $source $3/$source
 		;;
 	esac
     done
@@ -125,26 +119,24 @@ install_flask_app() (
 
 install_virtualenv() (
     assert [ $# -eq 2 ]
-    assert [ -n "$1" ]
-    source_dir="$1"
-    target_dir="$2"
-    check_permissions "$target_dir"
+    assert [ -n "$1" -a -n "$2" ]
+    check_permissions "$2"
 
     if [ $dryrun = false ]; then
-	assert [ -r "$source_dir" ]
-	printf "Installing files in %s\n" "$target_dir"
-	mkdir -p $target_dir
-	rsync -a "$source_dir"/* $target_dir
+	assert [ -r "$1" ]
+	printf "Installing files in %s\n" "$2"
+	mkdir -p $2
+	rsync -a "$1"/* $2
     fi
 )
 
-install_service() (
+install_service() {
     create_dirs $APP_DIR $APP_ETCDIR $APP_VARDIR
     install_file 600 .env $APP_DIR/.env
     install_flask_app 644 app $APP_DIR
     install_virtualenv $VENV_FILENAME-$APP_NAME $APP_DIR/$VENV_FILENAME
     change_owner $APP_DIR $APP_VARDIR
-    create_uwsgi_ini $APP_CONFIG app.ini $UWSGI_VARS
+    generate_service_ini $APP_CONFIG app.ini "$UWSGI_VARS"
     create_symlinks $APP_CONFIG $UWSGI_APPDIRS
 
     case "$kernel_name" in
@@ -152,7 +144,7 @@ install_service() (
 	    control_launch_agent load
 	    ;;
     esac
-)
+}
 
 start_service() (
     restart_service=false
@@ -164,7 +156,7 @@ start_service() (
     fi
 
     # Initialize database by invoking app module
-    $sh $script_dir/run.sh python3  -m app init-db
+    $sh "$script_dir/run.sh" python3  -m app init-db
 
     if [ $signal_received = true ]; then
 	case "$kernel_name" in
@@ -217,7 +209,7 @@ for dryrun in true false; do
     "$script_dir/install-uwsgi.sh" $dryrun
 
     if [ $dryrun = false ]; then
-	if ! $sh $script_dir/create-virtualenv.sh $venv_filename; then
+	if ! $sh "$script_dir/create-virtualenv.sh" $venv_filename; then
 	    abort "%s: Unable to create virtual environment\n" "$0"
 	fi
     fi
