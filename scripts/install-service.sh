91c19e922e9fef075dab781174d6ed879d73d9cb
Michael G. Morey <mgmorey@gmail.com>
2019-05-21 11:05:52 -0700

    Reduce parameter WAIT_DEFAULT value from 5 to 2 seconds.

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-WAIT_DEFAULT=5
+WAIT_DEFAULT=2
 WAIT_RESTART=10
 
 abort() {
