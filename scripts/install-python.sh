2741f247bb374b01f93c7cd0327b5be0a6b018c6
Michael G. Morey <mgmorey@gmail.com>
2019-05-28 13:49:55 -0700

    Abort script install-python.sh if run as a privileged user or root.

--- scripts/install-python.sh
+++ scripts/install-python.sh
@@ -52,6 +52,10 @@ if [ $# -gt 1 ]; then
     abort "%s: Too many arguments\n" "$0"
 fi
 
+if [ $(id -u) -eq 0 ]; then
+    abort "%s: Must be run as a non-privileged user\n" "$0"
+fi
+
 script_dir=$(get_realpath "$(dirname "$0")")
 
 . "$script_dir/common-parameters.sh"
