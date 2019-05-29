a1c913d7416b358f701ac1c192af4a6ab3cd2c28
Michael G. Morey <mgmorey@gmail.com>
2019-05-29 10:55:27 -0700

    Refactor AWK program in shell function print_table().

--- scripts/common-functions.sh
+++ scripts/common-functions.sh
@@ -260,30 +260,24 @@ print_table() {
 	function truncate(s) {
 	    return substr(s, 1, columns)
 	}
-	  BEGIN {if (columns < 80)
-		     columns = 80;
-		 if (columns > 240)
-		     columns = 240;
-		 dashes = "";
-		 for (i = 0; i < columns; i++)
-		     dashes = dashes "-";
-		 equals = "";
-		 for (i = 0; i < columns; i++)
-		     equals = equals "="}
-	NR == 1 {line1 = $0}
-	NR == 2 {printf("%s\n", truncate(equals));
-		 if (header)
-		     printf("%s\n%s\n%s\n",
-			    truncate(header),
-			    truncate(dashes),
-			    truncate(line1));
-		 else
-		     printf("%s\n%s\n",
-			    truncate(line1),
-			    truncate(dashes))}
-	NR >= 2 {printf("%s\n", truncate($0))}
-	    END {if (footer)
-		     printf("%s\n", truncate(equals))}'
+	BEGIN   { if (columns < 80)
+		      columns = 80;
+		  if (columns > 240)
+		      columns = 240;
+		  equals = dashes = "";
+		  for (i = 0; i < columns; i++) {
+		      dashes = dashes "-";
+		      equals = equals "="}
+		  header = truncate(header) }
+	NR == 1 { line1 = truncate($0) }
+	NR == 2 { printf("%s\n", truncate(equals));
+		  if (header)
+		      printf("%s\n%s\n%s\n", header, dashes, line1)
+		  else
+		      printf("%s\n%s\n", line1, dashes)}
+	NR >= 2 { printf("%s\n", truncate($0)) }
+	    END { if (footer)
+		      printf("%s\n", equals) }'
 }
 
 reset_home_directory() {
