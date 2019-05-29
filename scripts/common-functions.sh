617b1d7db4ea4caa5a411e5d72e06e3e4d0fd6c0
Michael G. Morey <mgmorey@gmail.com>
2019-05-29 13:16:12 -0700

    Refactor function print_table() to use AWK script print-table.awk.

--- scripts/common-functions.sh
+++ scripts/common-functions.sh
@@ -256,28 +256,9 @@ print_logs() {
 }
 
 print_table() {
-    awk -v columns="${COLUMNS-96}" -v header="$1" -v footer="${2-1}" '
-	function truncate(s) {
-	    return substr(s, 1, columns)
-	}
-	BEGIN   { if (columns < 80)
-		      columns = 80;
-		  if (columns > 240)
-		      columns = 240;
-		  equals = dashes = "";
-		  for (i = 0; i < columns; i++) {
-		      dashes = dashes "-";
-		      equals = equals "="}
-		  header = truncate(header) }
-	NR == 1 { line1 = truncate($0) }
-	NR == 2 { printf("%s\n", equals);
-		  if (header)
-		      printf("%s\n%s\n%s\n", header, dashes, line1)
-		  else
-		      printf("%s\n%s\n", line1, dashes)}
-	NR >= 2 { printf("%s\n", truncate($0)) }
-	END     { if (footer)
-		      printf("%s\n", equals) }'
+    "$script_dir/print-table.awk" -v header="$1" \
+				  -v footer="${2-1}" \
+				  -v columns="${COLUMNS-96}"
 }
 
 reset_home_directory() {
