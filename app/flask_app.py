3f2f912fac4d53622edd79e505315f60ac275d02
Michael G. Morey <mgmorey@gmail.com>
2019-05-30 14:34:02 -0700

    Refactor to combine functions for the /stadium/event resource.

--- app/flask_app.py
+++ app/flask_app.py
@@ -19,80 +19,76 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 app = create_app(__name__)  # pylint: disable=invalid-name
 
 
-@app.route('/stadium/event', methods=['PUT'])
-def add_or_replace_event():
-    """Add (or replace) an event to (or on) the calendar."""
+@app.route('/stadium/event', methods=['GET', 'DELETE', 'PUT'])
+def event():
+    """Add, retrieve or replace an event."""
 
-    if not request.json:
-        abort(400)
+    if request.method == 'GET':
+        event_name = request.args.get('name')
 
-    if set(request.json.keys()) != {'command', 'event', 'total'}:
-        abort(400)
+        if not event_name:
+            abort(400)
 
-    if request.json['command'] not in {'add_event', 'replace_event'}:
-        abort(400)
+        query = db.session.query(Events)
+        query = query.filter(Events.name == event_name)
+        event = query.first()
 
-    event_name = request.json['event']
-    event_total = request.json['total']
-    query = db.session.query(Events)
-    query = query.filter(Events.name == event_name)
-    query.delete()
-    event = Events(name=event_name, sold=0, total=event_total)
-    db.session.add(event)
-    try:
-        db.session.commit()
-    except IntegrityError as error:
-        logging.error("Error adding event: %s", str(error))
-        abort(400, 'Duplicate event')
-    else:
-        return jsonify({'event_name': event_name})
+        if not event:
+            abort(404)
 
+        return jsonify({'event': event.name})
+    elif request.method == 'DELETE':
+        event_name = request.args.get('name')
 
-@app.route('/stadium/event', methods=['GET'])
-def get_event():
-    """Retrieve an event from the calendar."""
+        if not event_name:
+            abort(400)
 
-    event_name = request.args.get('name')
+        query = db.session.query(Events)
+        query = query.filter(Events.name == event_name)
+        query.delete()
+        try:
+            db.session.commit()
+        except IntegrityError as error:
+            logging.error("Error removing event: %s", str(error))
+            abort(400, 'No such event')
+        else:
+            return jsonify({'event_name': event_name})
+    elif request.method == 'PUT':
+        if not request.json:
+            abort(400)
 
-    if not event_name:
-        abort(400)
+        if set(request.json.keys()) != {'command', 'event', 'total'}:
+            abort(400)
 
-    query = db.session.query(Events)
-    query = query.filter(Events.name == event_name)
-    return jsonify({'event': query})
+        if request.json['command'] not in {'add_event', 'replace_event'}:
+            abort(400)
+
+        event_name = request.json['event']
+        event_total = request.json['total']
+        query = db.session.query(Events)
+        query = query.filter(Events.name == event_name)
+        query.delete()
+        event = Events(name=event_name, sold=0, total=event_total)
+        db.session.add(event)
+        try:
+            db.session.commit()
+        except IntegrityError as error:
+            logging.error("Error adding event: %s", str(error))
+            abort(400, 'Duplicate event')
+        else:
+            return jsonify({'event_name': event_name})
 
 
 @app.route('/stadium/events', methods=['GET'])
-def get_events():
-    """Retrieve a list of events from the calendar."""
+def events():
+    """Retrieve a list of all events."""
 
     events = [e.name for e in db.session.query(Events).all()]
     return jsonify({'events': events})
 
 
-@app.route('/stadium/event', methods=['DELETE'])
-def remove_event():
-    """Remove an event from the calendar."""
-
-    event_name = request.args.get('name')
-
-    if not event_name:
-        abort(400)
-
-    query = db.session.query(Events)
-    query = query.filter(Events.name == event_name)
-    query.delete()
-    try:
-        db.session.commit()
-    except IntegrityError as error:
-        logging.error("Error removing event: %s", str(error))
-        abort(400, 'No such event')
-    else:
-        return jsonify({'event_name': event_name})
-
-
 @app.route('/stadium/tickets', methods=['POST'])
-def request_tickets():
+def tickets():
     """Request one or more tickets for an event."""
     max_count = 10
     min_count = 1
