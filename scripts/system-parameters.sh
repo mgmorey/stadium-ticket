3605730dfdef5187c882b658ca96ce2236e9e537
Michael G. Morey <mgmorey@gmail.com>
2019-05-30 20:17:08 -0700

    Set default ps command "ps axo user,pid,ppid,lstart,tty,command".

--- scripts/system-parameters.sh
+++ scripts/system-parameters.sh
@@ -218,7 +218,7 @@ configure_system_defaults() {
     # Set ps command format and command column
 
     if [ -z "${UWSGI_PS-}" ]; then
-	UWSGI_PS="ps axo user,pid,ppid,lstart,tname,command"
+	UWSGI_PS="ps axo user,pid,ppid,lstart,tty,command"
 	UWSGI_PS_COL=10
     fi
 
