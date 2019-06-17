ff113968dbf45baa383a86cd22351b5951b6d39f
Michael G. Morey <mgmorey@gmail.com>
2019-06-17 13:41:49 -0700

    Remove extraneous double-quotes surrounding parameter.

--- scripts/install-service.sh
+++ scripts/install-service.sh
@@ -115,7 +115,7 @@ generate_service_ini() {
 get_realpath() (
     assert [ $# -eq 1 ]
     assert [ -n "$1" ]
-    assert [ -d "$1" ]
+    assert [ -d $1 ]
     realpath=$(which realpath)
 
     if [ -n "$realpath" ]; then
