0e396bd5e93703dda0c1c4e59e023a70609dbfd2
Michael G. Morey <mgmorey@gmail.com>
2019-06-18 13:45:54 -0700

    Modify comment at top of script system-parameters.sh.

--- scripts/system-parameters.sh
+++ scripts/system-parameters.sh
@@ -1,6 +1,6 @@
 # -*- Mode: Shell-script -*-
 
-# system-parameters.sh: system configuration parameters
+# system-parameters.sh: system configuration functions and parameters
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
