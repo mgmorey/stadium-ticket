06d3cfccae4fde7af2b5fcf0f3592a0bdb4930fd
Michael G. Morey <mgmorey@gmail.com>
2019-06-20 09:13:11 -0700

    Add new script files disable-service.sh enable-service.sh.

---
+++ scripts/enable-service.sh
@@ -0,0 +1,60 @@
+#!/bin/sh -eu
+
+# enable-service.sh: run application as a service using uWSGI
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+abort() {
+    printf "$@" >&2
+    exit 1
+}
+
+assert() {
+    "$@" || abort "%s: Assertion failed: %s\n" "$0" "$*"
+}
+
+enable_service() {
+    if [ $UWSGI_RUN_AS_SERVICE = false ]; then
+	return 0
+    elif ! is_service_installed; then
+	return 0
+    fi
+
+    for dryrun in false; do
+	control_service enable
+    done
+}
+
+get_realpath() (
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+    assert [ -d $1 ]
+    realpath=$(which realpath)
+
+    if [ -n "$realpath" ]; then
+	$realpath "$1"
+    elif expr "$1" : '/.*' >/dev/null; then
+	printf "%s\n" "$1"
+    else
+	printf "%s\n" "$PWD/${1#./}"
+    fi
+)
+
+script_dir=$(get_realpath "$(dirname "$0")")
+
+. "$script_dir/common-parameters.sh"
+. "$script_dir/common-functions.sh"
+. "$script_dir/system-parameters.sh"
+. "$script_dir/system-functions.sh"
+
+configure_system
+enable_service
