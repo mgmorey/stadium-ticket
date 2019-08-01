bfbe939b5482fe092cf7800db176a83c67231966
Michael G. Morey <mgmorey@gmail.com>
2019-08-01 08:29:29 -0700

    Correct name of native uWSGI package on FreeBSD platforms.

--- scripts/get-uwsgi-packages.sh
+++ scripts/get-uwsgi-packages.sh
@@ -21,7 +21,7 @@ DARWIN_PKGS=":%s-uwsgi"
 DEBIAN_9_PKGS="setpriv uwsgi uwsgi-plugin-%s"
 DEBIAN_10_PKGS="util-linux uwsgi uwsgi-plugin-%s"
 
-FREEBSD_PKGS="uwsgi-%s"
+FREEBSD_PKGS="uwsgi"
 
 OPENSUSE_PKGS="system-user-wwwrun util-linux uwsgi uwsgi-%s"
 
