d582693fed4e5ecb117bee5f4841d027a1c32796
Michael G. Morey <mgmorey@gmail.com>
2019-08-27 10:34:45 -0700

    Refactor scripts to get lists of development packages to install.

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-CATEGORIES="devel http-client sqlite venv"
+CATEGORIES="devel flask http-client python-devel sqlite venv"
 
 abort() {
     printf "$@" >&2
--- scripts/get-devel-packages.sh
+++ scripts/get-devel-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -eu
 
-# get-devel-packages: get list of Python development packages
+# get-devel-packages: get list of development packages
 # Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
 
 # This program is free software: you can redistribute it and/or modify
