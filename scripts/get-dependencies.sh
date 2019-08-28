d6367725c5593c43e219a999ea41352f8b8e9d53
Michael G. Morey <mgmorey@gmail.com>
2019-08-28 12:05:13 -0700

    Remove redundant script scripts/get-venv-packages.sh.

--- scripts/get-dependencies.sh
+++ scripts/get-dependencies.sh
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-CATEGORIES="devel http-client python-devel python-flask sqlite venv"
+CATEGORIES="devel http-client python-devel python-flask sqlite"
 
 abort() {
     printf "$@" >&2
