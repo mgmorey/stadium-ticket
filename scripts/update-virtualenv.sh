56124bfebf8fba0717a787589feb94dd04cca684
Michael G. Morey <mgmorey@gmail.com>
2019-08-28 13:36:54 -0700

    Upgrade virtualenv as well as pip in create_virtualenv_via_pipenv().

--- scripts/update-virtualenv.sh
+++ scripts/update-virtualenv.sh
@@ -31,7 +31,7 @@ assert() {
 
 create_virtualenv_via_pipenv() {
     if ! $pipenv --venv >/dev/null 2>&1; then
-	upgrade_via_pip pip pipenv
+	upgrade_via_pip pip pipenv virtualenv
 
 	if pyenv --version >/dev/null 2>&1; then
 	    python=$(find_system_and_user_python)
