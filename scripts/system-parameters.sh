506f0ff6793d388f935ad824b23cfc693051df93
Michael G. Morey <mgmorey@gmail.com>
2019-08-31 14:20:22 -0700

    Clarify comment in shell function configure_bsd().

--- scripts/system-parameters.sh
+++ scripts/system-parameters.sh
@@ -32,7 +32,7 @@ awk_uwsgi() {
 }
 
 configure_bsd() {
-    # Set ps command format and column width
+    # Set ps command format and column number
     PS_COLUMN=10
     PS_FORMAT=pid,ppid,user,tt,lstart,command
 }
