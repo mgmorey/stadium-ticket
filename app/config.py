d4e5daa4945c3a029fdcb22542814762e9039552
Michael G. Morey <mgmorey@gmail.com>
2019-12-06 17:49:32 -0800

    Add new Python module file config.py.

---
+++ app/config.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""Define methods to parse application configuration file."""
+
+import configparser
+import os
+
+FILENAME = 'app.ini'
+
+
+def _get_pathname(dirname: str):
+    dirname = os.path.dirname(os.path.dirname(dirname))
+    pathname = os.path.join(dirname, FILENAME)
+    return pathname
+
+
+def get_config(dirname: str):
+    """Return application configuration."""
+    config = configparser.ConfigParser()
+    pathname = _get_pathname(dirname)
+    config.read(pathname)
+    return config
+
+
+def get_name(app_config):
+    """Return application name."""
+    return app_config['names']['name']
+
+
+def get_schema(app_config):
+    """Return application schema."""
+    return app_config['names']['schema']
+
+
+def get_vardir(app_config):
+    """Return application data directory."""
+    return app_config['names']['vardir']
