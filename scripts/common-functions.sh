cf06f1beec5c43124d86e8d118d551f21c1256e2
Michael G. Morey <mgmorey@gmail.com>
2019-12-11 13:48:04 -0800

    Pass positional argument "$1" to get_profile_path()

--- scripts/common-functions.sh
+++ scripts/common-functions.sh
@@ -51,9 +51,9 @@ get_home_directory() {
 get_profile_path() {
     path=$PATH
 
-    for prefix in "$1" "$1/.local" "$1/.pyenv"; do
-	if is_to_be_included "$prefix/bin" "$path"; then
-	   path="$prefix/bin:$path"
+    for dir in "$1/bin" "$1/.local/bin" "$1/.pyenv/bin" "$2"; do
+	if is_to_be_included "$dir" "$path"; then
+	   path="$dir:$path"
 	fi
     done
 
@@ -149,7 +149,7 @@ run_unpriv() (
 
 set_user_profile() {
     home="$(get_home_directory $(get_user_name))"
-    path="$(get_profile_path "$home")"
+    path="$(get_profile_path "$home" "$1")"
 
     if [ "$HOME" != "$home" ]; then
 	if [ "${ENV_VERBOSE-false}" = true ]; then
