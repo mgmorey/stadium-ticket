815021fcc8d94b211759514c6a8b4baa81ccb2ba
Michael G. Morey <mgmorey@gmail.com>
2020-03-29 18:43:58 -0700

    Refactor module datafile.py using additional methods.

--- app/datafile.py
+++ app/datafile.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Define methods to generate a database file pathname."""
+"""Define methods to generate a datafile pathname."""
 
 import configparser
 import os
@@ -12,29 +12,27 @@ FLASK_DATADIR = {
 
 
 def get_datafile(config: configparser.ConfigParser, dialect: str):
-    """Return the default database pathname."""
-    dirname = _get_dirname(config['app']['name'])
-    instance = config['database']['instance']
-    filename = '.'.join([instance, dialect])
+    """Return the default datafile pathname."""
+    dirname = _get_dirname(config)
+    filename = _get_filename(config, dialect)
     return os.path.join(dirname, filename)
 
 
-def _get_dirname(app_name: str):
-    """Return a database directory name."""
+def _get_dirname(config: configparser.ConfigParser):
+    """Return the default datafile directory name."""
     dirs = []
-    home = os.getenv('HOME')
-    tmpdir = os.getenv('TMPDIR')
+    home_dir = os.getenv('HOME')
+    temp_dir = os.getenv('TMPDIR')
 
     if not _is_development():
-        flask_datadir = FLASK_DATADIR.get(sys.platform, FLASK_DATADIR[None])
-        dirs.append(os.path.join(flask_datadir, app_name))
+        dirs.append(os.path.join(_get_flask_datadir(), config['app']['name']))
 
-    if home:
-        dirs.append(os.path.join(home, '.local', 'share'))
-        dirs.append(home)
+    if home_dir is not None:
+        dirs.append(os.path.join(home_dir, '.local', 'share'))
+        dirs.append(home_dir)
 
-    if tmpdir:
-        dirs.append(tmpdir)
+    if temp_dir is not None:
+        dirs.append(temp_dir)
 
     dirs.append(os.path.join(os.path.sep, 'tmp'))
 
@@ -45,7 +43,17 @@ def _get_dirname(app_name: str):
     return ''
 
 
+def _get_filename(config: configparser.ConfigParser, dialect: str):
+    """Return the default datafile filename."""
+    instance = config['database']['instance']
+    return '.'.join([instance, dialect])
+
+
+def _get_flask_datadir():
+    return FLASK_DATADIR.get(sys.platform, FLASK_DATADIR.get(None))
+
+
 def _is_development():
-    """Return true if running in a development environment."""
+    """Return true if Flask is running in a development environment."""
     return (os.getenv('FLASK_ENV', 'production') == 'development' or
             os.getenv('WERKZEUG_RUN_MAIN', 'false') == 'true')
